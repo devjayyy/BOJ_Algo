2c9ed5dea7fae1f4f01ff7b17cca5b4d62771d25
Kyoungmin Nam <rudalswhdk12@naver.com>
2022-01-17 00:52:37 +0900

    Initial Commit

---
+++ BOJ_Algo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BOJ_Algo
+//
+//  Created by 남경민 on 2022/01/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
